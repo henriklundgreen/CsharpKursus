474167b7557a4f2a2d56fa5f591b8fc444bc81ba
Henrik <hlu@itrelation.dk>
2017-08-29 15:31:04 +0200

    .

--- Module03_ValueTypes/Program.cs
+++ Module03_ValueTypes/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Module03_ValueTypes
+﻿using System;
+
+namespace Module03_ValueTypes
 {
     class Program
     {
@@ -7,6 +9,18 @@
 
             if (System.Diagnostics.Debugger.IsAttached)
             {
+                //System.Int32 i;
+                //System.Decimal komma;
+                int i;
+                decimal komma;
+
+                long l;
+                DateTime dato;
+                bool b;
+                dato = DateTime.Now;
+                DateTime jul = new DateTime(2017, 12, 24);
+                TimeSpan ts = jul.Subtract(dato);
+
                 System.Console.Write("Press any key to continue . . . ");
                 System.Console.ReadKey();
             }
